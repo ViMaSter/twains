11802aa9045fd7b4867d71d997e11dfd2ab09e83
Vincent Mahnke <vincent@mahn.ke>
2026-07-05 14:10:28 +0200

    chore: More verbose logging

--- Scripts/Interactables/PickupBox3D.cs
+++ Scripts/Interactables/PickupBox3D.cs
@@ -77,7 +77,7 @@ public partial class PickupBox3D : RigidBody3D, IInteractable
 			throw new InvalidOperationException($"PickupBox3D: Could not find CharacterBodyPawn3D in parent-parent chain for entering body '{body.Name}'.");
 		}
 
-		GD.Print($"PickupBox3D: Something entered trigger volume: {body.Name}");
+		GD.Print($"{this.Name}: Something entered trigger volume: {body.Name}");
 		pawn.SetInteractable(this);
 	}
 
@@ -89,17 +89,17 @@ public partial class PickupBox3D : RigidBody3D, IInteractable
 			throw new InvalidOperationException($"PickupBox3D: Could not find CharacterBodyPawn3D for exiting body '{body.Name}'.");
 		}
 
-		GD.Print($"PickupBox3D: Something left trigger volume: {body.Name}");
+		GD.Print($"{this.Name}: Something left trigger volume: {body.Name}");
 		pawn.ClearInteractable();
 	}
 
 	bool IInteractable.Use(Node3D user)
 	{
-		GD.Print($"PickupBox3D: Use() called on '{Name}' by '{user.Name}'");
+		GD.Print($"{this.Name}: Use() called by '{user.Name}'");
 		PickupPawn3D pickupPawn = user.FindChildOfType<PickupPawn3D>();
 		if (pickupPawn is null)
 		{
-			GD.PushWarning($"PickupBox3D '{Name}': Use() called by '{user.Name}', but no PickupPawn3D found in parent-parent chain.");
+			GD.PushWarning($"{this.Name}: Use() called by '{user.Name}', but no PickupPawn3D found in parent-parent chain.");
 			return false;
 		}
 
